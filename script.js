2ac233a42da5158c7da051b3160b6a2a197bf24d
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-05-11 23:54:41 +0200

    card on click open

--- script.js
+++ script.js
@@ -12,3 +12,17 @@
   function randomInRange(min, max) {
     return Math.random() * (max - min) + min;
   }
+
+
+  const card = document.querySelector('.card');
+
+card.addEventListener('click', () => {
+  card.classList.toggle('card-clicked');
+});
+
+function handleClick() {
+    var card = document.querySelector(".card");
+    if (!card.classList.contains("clicked")) {
+      card.classList.add("clicked");
+    }
+}
