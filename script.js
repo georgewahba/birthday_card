3dfe9a2ae443ff2b7b00ab8c9d38a730a6d3f4b7
George Wahba <54804560+georgewahba@users.noreply.github.com>
2023-05-14 16:54:32 +0200

    birthday song

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+const audio = new Audio('birthday.mp3');
 
-  function randomDirection(){
-   confetti({
+function randomDirection(){
+  confetti({
     angle: randomInRange(55, 125),
     spread: randomInRange(50, 70),
     particleCount: randomInRange(50, 100),
     origin: { y: 0.6 }
   }); 
-  }
-  
-  
-  function randomInRange(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+}
 
 
-  const card = document.querySelector('.card');
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+
+const card = document.querySelector('.card');
 
 card.addEventListener('click', () => {
   card.classList.toggle('card-clicked');
+  audio.play();
+
 });
 
 function handleClick() {
